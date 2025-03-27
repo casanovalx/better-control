5f192d4e87cf5dd0c399ad53403a74f98ec065eb
quantumvoid <pranavrajesh009@gmail.com>
2025-03-27 12:41:56 +0530

    Update better_control.py

--- src/better_control.py
+++ src/better_control.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python3
+
 import os
 import subprocess
-import gi  
+import gi  # type: ignore
 import sys
 import threading
-import requests 
 from utils.arg_parser import ArgParse, sprint
 from utils.pair import Pair
+from utils.logger import LogLevel, Logger
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk  
+from gi.repository import Gtk  # type: ignore
 from ui.main_window import BetterControl
 from utils.dependencies import check_all_dependencies
 from tools.bluetooth import restore_last_sink
 
+
 if __name__ == "__main__":
-    arg_parser = ArgParse(sys.argv)  
+    arg_parser = ArgParse(sys.argv)
 
     if arg_parser.find_arg(("-h", "--help")):
         arg_parser.print_help_msg(sys.stdout)
@@ -23,31 +26,37 @@ if __name__ == "__main__":
         sprint(sys.stdout, "5.3")
         exit(0)
 
-    if arg_parser.find_arg(("-f", "--force")) and not check_all_dependencies():
-        print("Missing required dependencies. Please install them and try again.")
-        exit(1)
+    logging = Logger(arg_parser)
 
-    # Prevents startup delay
-    audio_thread = threading.Thread(target=restore_last_sink, daemon=True)
+    if arg_parser.find_arg(("-f", "--force")) and not check_all_dependencies(logging):
+        logging.log(
+            LogLevel.Error,
+            "Missing required dependencies. Please install them and try again.",
+        )
+
+    # ? Prevents startup delay
+    audio_thread = threading.Thread(target=restore_last_sink, args=(logging,), daemon=True)
     audio_thread.start()
 
     try:
-        win = BetterControl(arg_parser, None)  # Pass None as logging is removed
+        win = BetterControl(arg_parser, logging)
         win.set_default_size(1000, 700)
         win.resize(1000, 700)
         win.connect("destroy", Gtk.main_quit)
         win.show_all()
 
-        # Check for updates
-        check_for_updates(win)
-
-        # Hyprland floating window rule
+        #? Hyprland shenanigans
         if "hyprland" in os.environ.get("XDG_CURRENT_DESKTOP", "").lower():
             subprocess.run(
-                ["hyprctl", "keyword", "windowrule", "float,class:^(better_control.py)$"]
+                [
+                    "hyprctl",
+                    "keyword",
+                    "windowrule",
+                    "float,class:^(better_control.py)$",
+                ]
             )
 
         Gtk.main()
     except Exception as e:
-        print(f"Error starting application: {e}")
+        logging.log(LogLevel.Error, f"Error starting application: {e}")
         sys.exit(1)
