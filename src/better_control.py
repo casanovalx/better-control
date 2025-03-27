45ef366d4142298fb60f2079d4c1817c0e515b24
quantumvoid <pranavrajesh009@gmail.com>
2025-03-27 12:08:13 +0530

    new feature to check updates

--- src/better_control.py
+++ src/better_control.py
@@ -3,7 +3,7 @@ import subprocess
 import gi  
 import sys
 import threading
-import logger
+import logging
 import requests  # GET THIS
 from utils.arg_parser import ArgParse, sprint
 from utils.pair import Pair
